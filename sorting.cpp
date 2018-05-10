d20aafe421477188c33f7e36da37ea8bd7e1ac46
Poorvaja2799 <poorvaja2799@gmail.com>
2018-05-10 23:38:18 +0530

    modifying sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -1,25 +1,70 @@
-void merge(int A[ ] , int start, int mid, int end) {
- //stores the starting position of both parts in temporary variables.
-int p = start ,q = mid+1;
-
-int Arr[end-start+1] , k=0;
-
-for(int i = start ;i <= end ;i++) {
-    if(p > mid)      //checks if first part comes to an end or not .
-       Arr[ k++ ] = A[ q++] ;
-
-   else if ( q > end)   //checks if second part comes to an end or not
-       Arr[ k++ ] = A[ p++ ];
-
-   else if( A[ p ] < A[ q ])     //checks which part has smaller element.
-      Arr[ k++ ] = A[ p++ ];
-
-   else
-      Arr[ k++ ] = A[ q++];
- }
-  for (int p=0 ; p< k ;p ++) {
-   /* Now the real array has elements in sorted manner including both 
-        parts.*/
-     A[ start++ ] = Arr[ p ] ;                          
-  }
+void main()
+{
+	int n, i;
+	cout<<"Enter the number of element to be sorted: ";
+	cin>>n;
+	int arr[n];
+	for(i = 0; i < n; i++)
+	{
+		cout<<"Enter element "<<i+1<<": ";
+		cin>>arr[i];
+	}
+	MergeSort(arr, 0, n-1);
+	// Printing the sorted elements
+	cout<<"\nSorted Elements ";
+	for (i = 0; i < n; i++)
+        cout<<arr[i]<<" "; 
 }
+void Merge(int *a, int low, int high, int mid)
+{
+	int i, j, k, temp[high-low+1];
+	i = low;
+	k = 0;
+	j = mid + 1;
+	while (i <= mid && j <= high)
+	{
+		if (a[i] < a[j])
+		{
+			temp[k] = a[i];
+			k++;
+			i++;
+		}
+		else
+		{
+			temp[k] = a[j];
+			k++;
+			j++;
+		}
+	}
+	while (i <= mid)
+	{
+		temp[k] = a[i];
+		k++;
+		i++;
+	}
+	while (j <= high)
+	{
+		temp[k] = a[j];
+		k++;
+		j++;
+	}
+ 	// Assign sorted data stored in temp[] to a[].
+	for (i = low; i <= high; i++)
+	{
+		a[i] = temp[i-low];
+	}
+}
+void MergeSort(int *a, int low, int high)
+{
+	int mid;
+	if (low < high)
+	{
+		mid=(low+high)/2;
+		// Split the data into two half.
+		MergeSort(a, low, mid);
+		MergeSort(a, mid+1, high); 
+		// Merge them to get sorted output.
+		Merge(a, low, high, mid);
+	}
+}
+ 
